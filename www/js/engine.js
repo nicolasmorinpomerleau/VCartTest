acdb5d47dcb0d08c24cd76f30ef1a1cdb30ad889
nicolas <nicolasmorinpomerleau@gmail.com>
2018-10-20 20:56:15 -0400

    OneSignal
    First modifications for OneSignal Push notifications
    Add OneSignal codes in to config.xml and engine.js files

--- www/js/engine.js
+++ www/js/engine.js
@@ -1,6 +1,28 @@
 var tentative = 0;
 var nouveuMessage;
 
+
+<!--For OneSignal push notifications-->
+<!--================================================================-->
+// Add to index.js or the first page that loads with your app.
+// For Intel XDK and please add this to your app.js.
+
+document.addEventListener('deviceready', function () {
+  // Enable to debug issues.
+  // window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
+  
+  var notificationOpenedCallback = function(jsonData) {
+    console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
+  };
+
+  window.plugins.OneSignal
+    .startInit("8388c0ef-1492-4892-9296-17091773564c")
+    .handleNotificationOpened(notificationOpenedCallback)
+    .endInit();
+}, false);
+<!--================================================================-->
+
+
 $(function(){
         if(localStorage.Email!=undefined && localStorage.code != undefined){
             nameID               = document.getElementById("name");
