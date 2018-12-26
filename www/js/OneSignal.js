0f6e236c0330e943b4345f9962b27f81b6e755ce
nicolas <nicolasmorinpomerleau@gmail.com>
2018-12-26 15:59:08 -0500

    OneSignal
    change onsignal.js de place
    add document.onload dans onsignal.js

--- www/js/OneSignal.js
+++ www/js/OneSignal.js
@@ -1,19 +1,21 @@
-<!--For OneSignal push notifications-->
-<!--================================================================-->
 // Add to index.js or the first page that loads with your app.
 // For Intel XDK and please add this to your app.js.
 
-document.addEventListener('deviceready', function () {
+//document.addEventListener('deviceready', function () {
+    $(document).ready(function(){
+
   // Enable to debug issues.
-  // window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
+   window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
   
   var notificationOpenedCallback = function(jsonData) {
     console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
   };
 
+    OneSignal.setLogLevel(OneSignal.LOG_LEVEL.DEBUG, OneSignal.LOG_LEVEL.DEBUG);
+    
   window.plugins.OneSignal
     .startInit("8388c0ef-1492-4892-9296-17091773564c")
     .handleNotificationOpened(notificationOpenedCallback)
     .endInit();
-}, false);
-<!--================================================================-->
+});
+//    , false);
