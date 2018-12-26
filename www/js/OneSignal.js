96ccf13a407af7b70d5df6338619fef367da3c72
nicolas <nicolasmorinpomerleau@gmail.com>
2018-12-26 16:56:52 -0500

    OneSignal.js

--- www/js/OneSignal.js
+++ www/js/OneSignal.js
@@ -1,7 +1,8 @@
 // Add to index.js or the first page that loads with your app.
 // For Intel XDK and please add this to your app.js.
 function onLoad() {
-    
+        alert("ready for Onesignal: avant le addEventListner");
+
 document.addEventListener("deviceready", function () {
 //    $(document).ready(function(){
 
@@ -14,7 +15,8 @@ document.addEventListener("deviceready", function () {
 
     OneSignal.setLogLevel(OneSignal.LOG_LEVEL.DEBUG, OneSignal.LOG_LEVEL.DEBUG);
     
-    window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
+//    window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
+    alert("ready for Onesignal: dans le AddEventListner");
   window.plugins.OneSignal
     .startInit("8388c0ef-1492-4892-9296-17091773564c")
     .handleNotificationOpened(notificationOpenedCallback)
