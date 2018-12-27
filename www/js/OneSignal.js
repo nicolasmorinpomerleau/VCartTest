ad15157bb56f1e0b48c96771baf1a959515fd53c
nicolas <nicolasmorinpomerleau@gmail.com>
2018-12-26 20:08:05 -0500

    add
    add Eventlistener
    remove onload() from index.html

--- www/js/OneSignal.js
+++ www/js/OneSignal.js
@@ -1,26 +1,21 @@
+
+
 // Add to index.js or the first page that loads with your app.
 // For Intel XDK and please add this to your app.js.
-function onLoad() {
-        alert("ready for Onesignal: avant le addEventListner");
 
-//document.addEventListener("deviceready", function () {
-//    $(document).ready(function(){
-
+document.addEventListener("deviceready", onDeviceReady, false);
   // Enable to debug issues.
-//   window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4}); removed lately
-  
-  var notificationOpenedCallback = function(jsonData) {
-    console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
-  };
+  // window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
+  function onDeviceReady(){
+    alert(" Device ready");
 
-//    OneSignal.setLogLevel(OneSignal.LOG_LEVEL.DEBUG, OneSignal.LOG_LEVEL.DEBUG); removed lately
-    
-//    window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
-    alert("ready");
-  window.plugins.OneSignal
-    .startInit("8388c0ef-1492-4892-9296-17091773564c")
-    .handleNotificationOpened(notificationOpenedCallback)
-    .endInit();
-}
-
-//                        , false);
+    var notificationOpenedCallback = function(jsonData) {
+        console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
+    };
+    alert(" Init ready");
+      window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
+      window.plugins.OneSignal
+        .startInit("8388c0ef-1492-4892-9296-17091773564c")
+        .handleNotificationOpened(notificationOpenedCallback)
+        .endInit();
+};
